5a0b2c1e6706fc352b20bec314379e7e23fa7462
Stefan Toman <git@stoman.de>
2016-06-30 23:21:02 +0200

    document the predictionquality function

--- functions/predictionquality.m
+++ functions/predictionquality.m
@@ -1,3 +1,8 @@
+%This function predicts the classes of some objects using kernel methods. X
+%and Y are the training data, lambda is the regularization parameter, K is
+%the kernel function and Xt and Yt are the test data. The return value is
+%the number of correct estimates.
+%Author: Stefan Toman (toman@tum.de)
 function correct = predictionquality(X, Y, lambda, K, Xt, Yt)
     predict = funpredict(X, Y, lambda, K);
     predictions = predict(Xt);
